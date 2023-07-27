160dfa267a7c352b1091714aab6fcb089cfea1f6
Byeong Min Cho <merrybmc@gmail.com>
2023-07-28 00:29:54 +0900

    타입스크립트 공부

---
+++ repeat/1-TYPES/1-1-basic.ts
@@ -0,0 +1,25 @@
+// number;
+const num: number = 1;
+console.log(num);
+
+//string
+const str: string = 'Hello';
+console.log(str);
+
+// boolean
+const bool: boolean = false;
+console.log(bool);
+
+// undefined
+let age: number | undefined;
+
+// null
+let person: string | null;
+
+// unknown
+let notSure: unknown = 0;
+console.log(notSure);
+notSure = 'hi';
+console.log(notSure);
+notSure = true;
+console.log(notSure);
