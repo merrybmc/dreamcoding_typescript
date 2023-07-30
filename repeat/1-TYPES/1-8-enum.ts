20cabb0536af267dfea4e8de8dea7795fff4949a
Byeong Min Cho <merrybmc@gmail.com>
2023-07-30 17:11:48 +0900

    20230730 study

---
+++ repeat/1-TYPES/1-8-enum.ts
@@ -0,0 +1,20 @@
+{
+  // JavaScript
+  const MAX_NUM = 6;
+  const MAX_STUDENTS_PER_CLASS = 10;
+  const MONDAY = 0;
+  const TUESDAY = 1;
+  const WEDNESDAY = 2;
+
+  const DAYS_ENUM = Object.freeze({ MONDAY: 0, TUESDAY: 1, WEDNESDAY: 2 });
+  const dayOfToday = DAYS_ENUM.MONDAY;
+
+  // TypeScript
+  enum DAYS {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+  }
+}
